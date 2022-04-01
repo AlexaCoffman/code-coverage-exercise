0fb404a4b90145be467479cfa4132c296f89759c
Alexa Coffman <coffman.alexa@gmail.com>
2022-04-01 13:55:19 -0700

    added tests for get_student_with_more_classes and completed function

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,13 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_empty_course_list():
+    courses = []
+    alexa = Student("Alexa", "Junior")
+    
+    assert alexa.courses == []
+
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -33,7 +40,7 @@ def test_summary():
 
     assert anne.summary() == "Anne Byron is a senior enrolled in 2 classes"
 
-def test_get_student_with_more_classes():
+def test_get_student_a_with_more_classes():
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
     ada = Student(
         "Ada Lovelace",
@@ -41,4 +48,21 @@ def test_get_student_with_more_classes():
         ["mathematics", "foundations of computing"]
     )
 
-    # TODO: write assertions
+    assert get_student_with_more_classes(ada, charles) == ada
+    
+def test_get_student_b_with_more_classes():
+    charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
+    ada = Student(
+        "Ada Lovelace",
+        "sophomore",
+        ["mathematics", "foundations of computing"]
+    )
+    assert get_student_with_more_classes(charles, ada) == ada
+
+def test_get_both_students_if_tied():
+    charles = Student("Charles Babbage", "senior", ["mechanical engineering", "Intro to Feminism"])
+    ada = Student(
+        "Ada Lovelace",
+        "sophomore",
+        ["mathematics", "foundations of computing"])
+    assert get_student_with_more_classes(charles, ada) == (charles, ada)
